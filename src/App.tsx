8620857905bd39f39043b4072e014cf5189675f6
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-09 11:14:30 -0300

    🎉 feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>Have a nice coding, Rian Oliveira ;)</p>
+    </>
+  );
+};
---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+import axios, { AxiosInstance } from "axios";
+
+export const api: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
